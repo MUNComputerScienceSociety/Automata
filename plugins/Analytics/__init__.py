c82981f486a23f5481b35e0a88cba589d0903913
Riley Flynn <riley@rileyflynn.me>
2019-07-07 19:25:36 -0230

    Persist message counts

--- plugins/Analytics/__init__.py
+++ plugins/Analytics/__init__.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from prometheus_client import Counter, Gauge
 
 from Plugin import AutomataPlugin
+from Globals import mongo_client
 
 MESSAGES = Counter("automata_message_count", "Total number of messages sent.", ["channel"])
 MEMBERS = Gauge("automata_member_count", "Number of members in the server", ["status"])
@@ -11,15 +12,11 @@ MEMBERS = Gauge("automata_member_count", "Number of members in the server", ["st
 class Analytics(AutomataPlugin):
     """Provides statistics and analystics tracking services."""
 
-    @commands.Cog.listener()
-    async def on_message(self, message: discord.Message):
-        """Process incoming messages, updating the relevant counters on Prometheus."""
-        if not isinstance(message.channel, discord.abc.PrivateChannel):
-            MESSAGES.labels(message.channel.name).inc(1)
-
-    @commands.Cog.listener()
-    async def on_member_update(self, before: discord.Member, after: discord.Member):
-        """Handle member update events, updating the relevant gague on Prometheus."""
+    def __init__(self, manifest, bot: commands.Bot):
+        super().__init__(manifest, bot)
+        self.message_counts = mongo_client.automata.analytics_message_counts
+    
+    def _set_member_count(self) -> None:
         totals = {
             "total": 0,
             "online": 0,
@@ -32,3 +29,28 @@ class Analytics(AutomataPlugin):
             totals["total"] += 1
         for label, value in totals.items():
             MEMBERS.labels(label).set(value)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        cursor = self.message_counts.find()
+        for channel in await cursor.to_list(length=None):
+            guild_channel = self.bot.get_guild(514110851016556567).get_channel(channel["channel_id"])
+            MESSAGES.labels(guild_channel.name).inc(channel["count"])
+        self._set_member_count()
+
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        """Process incoming messages, updating the relevant counters on Prometheus."""
+        if not isinstance(message.channel, discord.abc.PrivateChannel):
+            existing_count = await self.message_counts.find_one({"channel_id": message.channel.id})
+            if existing_count is not None:
+                await self.message_counts.update_one({"channel_id": message.channel.id}, {"$inc": {"count": 1}})
+            else:
+                await self.message_counts.insert_one({"channel_id": message.channel.id, "count": 1})
+            MESSAGES.labels(message.channel.name).inc(1)
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before: discord.Member, after: discord.Member):
+        """Handle member update events, updating the relevant gague on Prometheus."""
+        self._set_member_count()
