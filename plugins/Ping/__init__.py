7782f11242edc556cb36e6249427d8d1d9518800
Jack Arthur Harrhy <me@jackharrhy.com>
2019-10-15 22:34:05 -0230

    remove __init__ and useless import

--- plugins/Ping/__init__.py
+++ plugins/Ping/__init__.py
@@ -2,14 +2,10 @@ import discord
 from discord.ext import commands
 
 from Plugin import AutomataPlugin
-from Globals import mongo_client
 
 
 class Ping(AutomataPlugin):
     """Pong"""
-
-    def __init__(self, manifest, bot: commands.Bot):
-        super().__init__(manifest, bot)
 
     @commands.command()
     async def ping(self, ctx: commands.Context, number_of_times: int = 0):
