1014968f2ff282d93342f63f172a38427c69aa11
Nathan Stevenson <nscstevenson@mun.ca>
2019-12-13 01:34:04 -0330

    Over9000
    Takes one of several DBZ character's names as arguments:
    Kakarot/Goku, Frieza, Vegeta. Sends a message in the discord chat
    with a primitive, single-line saiyan text emoji and a quote with
    the character's name
    
    If no arguments are passed, the plugin defaults to Goku.
    If an invalid argument is passed, an elipsis replaces the
    would-be quote and name.
    
    May add more characters and quotes in the future. Other features in
    mind as well. Depends on the free time I have for tomfoolery and
    shenanigans.

---
+++ plugins/Over9000/__init__.py
@@ -0,0 +1,42 @@
+﻿from random import choice, randint
+import discord
+from discord.ext import commands
+
+from .quotes import DBZQuotes
+
+from Plugin import AutomataPlugin
+
+
+class Over9000(AutomataPlugin):
+    """Kakarot"""
+
+    @commands.command()
+    async def over9000(self, ctx: commands.Context, dbz_char: str = 'goku'):
+        """Replies with a super saiyan emoji and random
+        Goku quote!
+
+        Takes character name as an argument.
+
+        """
+
+
+        dbz_quotes = DBZQuotes()
+
+        if dbz_char.upper() in ('GOKU', 'KAKAROT'):
+            quotes = dbz_quotes.goku()
+            quote = choice(quotes)
+            await ctx.send(f"ᕙ(⇀‸↼‶)ᕗ\n**Goku:** {quote}")
+
+        elif dbz_char.upper() == 'FRIEZA':
+            quotes = dbz_quotes.frieza()
+            quote = choice(quotes)
+            await ctx.send(f"ᕙ(⇀‸↼‶)ᕗ\n**Frieza:** {quote}")
+        
+        elif dbz_char.upper() == 'VEGETA':
+            quotes = dbz_quotes.vegeta()
+            quote = choice(quotes)
+            await ctx.send(f"ᕙ(⇀‸↼‶)ᕗ\n**Vegeta:** {quote}")
+
+        else:
+            """An invalid DBZ character was specified."""
+            await ctx.send(f"ᕙ(⇀‸↼‶)ᕗ\n...")
