1b4379514040243d84f363f9f832c5126c42c829
Zach Vaters <zachvaters@gmail.com>
2020-06-07 18:56:19 -0230

    defined a current day func formatted

--- plugins/TodayAtMun/__init__.py
+++ plugins/TodayAtMun/__init__.py
@@ -1,33 +1,25 @@
 import discord
+from os import path
 from discord.ext import commands
+from plugins.TodayAtMun.Month import month
 from datetime import datetime
-from month import month
-
-#from Plugin import AutomataPlugin
 
 
-class TodayAtMun():
+from Plugin import AutomataPlugin
+
+
+class TodayAtMun(AutomataPlugin):
     """
         Provides Significant Dates on the Mun Calendar
     """
 
-    fileName = "diary.txt"
-    date = str(datetime.now().strftime("%Y-%#m-%#d"))
+    def __init__(self, manifest, bot):
+        super().__init__(manifest, bot)
+        with open(path.join(self.manifest["path"], "diary.txt"), "r") as f:
+            self.info = f
 
-    dice = date.split("-")
-
-    currYear = dice[0]
-    currMonth = int(dice[1])
-    currDay = dice[2]
-
-    lookUp = f"{month[currMonth]} {currDay}, {currYear}"
-    
-    print(lookUp)
-    fileReader = open(fileName, "r")
-    # fileName.close()
-
-    @commands.Command()
-    async def getCurrDate(self):
+    @commands.command()
+    async def getCurrDate(self, ctx):
         date = str(datetime.now().strftime("%Y-%#m-%#d"))
 
         dice = date.split("-")
@@ -38,5 +30,4 @@ class TodayAtMun():
 
         lookUp = f"{month[currMonth]} {currDay}, {currYear}"
 
-        send(lookUp)
-    
+        await ctx.send(lookUp)
