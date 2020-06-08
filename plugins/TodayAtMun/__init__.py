3b4fceba769ad290f527097c76b9bba962544e65
Zach Vaters <zachvaters@gmail.com>
2020-06-08 18:00:54 -0230

    Compacted code added dow

--- plugins/TodayAtMun/__init__.py
+++ plugins/TodayAtMun/__init__.py
@@ -3,31 +3,35 @@ from os import path
 from discord.ext import commands
 from plugins.TodayAtMun.Month import month
 from datetime import datetime
-
+from json import load
 
 from Plugin import AutomataPlugin
 
 
 class TodayAtMun(AutomataPlugin):
     """
-        Provides Significant Dates on the Mun Calendar
+        Provides Info Of Significant Dates on the Mun Calendar
     """
 
     def __init__(self, manifest, bot):
         super().__init__(manifest, bot)
-        with open(path.join(self.manifest["path"], "diary.txt"), "r") as f:
-            self.info = f
+        with open(path.join(self.manifest["path"], "diary.json"), "r") as f:
+            self.info = load(f)
+
+
+    def getCurrDate(self):
+        """ Provides Current Date formatted to Muns style."""
+
+        date = str(datetime.now().strftime("%Y-%#m-%#d-%A")).split("-")
+
+        currYear = date[0]
+        currMonth = int(date[1])
+        currDay = date[2]
+        currDayOfWeek = date[3]
+
+        return f"{month[currMonth]} {currDay}, {currYear}, {currDayOfWeek}"
 
     @commands.command()
-    async def getCurrDate(self, ctx):
-        date = str(datetime.now().strftime("%Y-%#m-%#d"))
-
-        dice = date.split("-")
-
-        currYear = dice[0]
-        currMonth = int(dice[1])
-        currDay = dice[2]
-
-        lookUp = f"{month[currMonth]} {currDay}, {currYear}"
-
-        await ctx.send(lookUp)
+    async def today(self, ctx):
+        date = self.getCurrDate()
+        print(f"Today is: {date}.")
