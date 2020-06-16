e53526f9231164e0f5f2cdd09391913e60c6b4ee
Zach Vaters <zachvaters@gmail.com>
2020-06-16 17:17:37 -0230

    style: 💄 help command description

--- plugins/TodayAtMun/__init__.py
+++ plugins/TodayAtMun/__init__.py
@@ -6,21 +6,21 @@ from Plugin import AutomataPlugin
 from plugins.TodayAtMun.diary_parser import diary_parser
 from plugins.TodayAtMun.today import Today
 
-class TodayAtMun(AutomataPlugin):
-    #Provides Brief Info Of Significant Dates on the Mun Calendar
 
+class TodayAtMun(AutomataPlugin):
     def __init__(self, manifest, bot):
         super().__init__(manifest, bot)
         self.Tod = Today()
 
     @commands.group()
     async def today(self, ctx):
+        """ Provides Brief Info Of Significant Dates on the Mun Calendar"""
         if not ctx.invoked_subcommand:
             await ctx.send("Please Provide Subcommand e.g !today [soon]")
 
     @today.command(name="next")
     async def today_next(self, ctx):
-        #Sends quick update on Muns Next Calendar Date on the Uni Diary
+        # Sends quick update on Muns Next Calendar Date on the Uni Diary
         self.Tod.set_current_date()
         self.Tod.findEvent(self.Tod.date)
         embed = discord.Embed(
@@ -33,13 +33,13 @@ class TodayAtMun(AutomataPlugin):
 
     @today.command(name="reset")
     async def today_reset(self, ctx):
-        #Re-Parses data from Mun Diary.
+        # Re-Parses data from Mun Diary.
         diary_parser()
         await ctx.send("Data Reset!")
 
     @today.command(name="later")
     async def today_after(self, ctx):
-        #Sends next date coming up on the univsersity diary
+        # Sends next date coming up on the univsersity diary
         self.Tod.set_current_date()
         self.Tod.findEvent(self.Tod.date)
         self.Tod.nextDay()
