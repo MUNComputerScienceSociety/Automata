e0a729b59707fed1d53e46b95253c0995d20f0f1
Jack Arthur Harrhy <me@jackharrhy.com>
2020-11-04 17:47:49 -0330

    state intents

--- Bot.py
+++ Bot.py
@@ -48,9 +48,13 @@ if not AUTOMATA_TOKEN:
     )
     exit(1)
 
+intents = discord.Intents.default()
+intents.members = True
+
 bot = commands.Bot(
     "!",
     description="A custom, multi-purpose moderation bot for the MUN Computer Science Society Discord server.",
+    intents=intents
 )
 
 
