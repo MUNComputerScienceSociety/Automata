ab493f077ad39083dd09f319c13f0230a26942ac
Hamzah Punjabi <hamzahqatar123@gmail.com>
2021-05-14 12:26:25 +0300

    Cat Bop implemented
    Should work

--- plugins/Vibe/__init__.py
+++ plugins/Vibe/__init__.py
@@ -4,14 +4,11 @@ from discord.ext import commands
 from Plugin import AutomataPlugin
 
 
-class Ping(AutomataPlugin):
-    """Pong"""
+class Vibe(AutomataPlugin):
+    """Cat Bop"""
 
     @commands.command()
-    async def ping(self, ctx: commands.Context, number_of_times: int = 0):
-        """Replies with a Pong, or many!"""
+    async def vibe(self, ctx: commands.Context):
+        """Replies with a Cat Bop Gif!"""
 
-        if number_of_times == 0:
-            await ctx.send("Pong!")
-        else:
-            await ctx.send(f"Pong! x{number_of_times}")
+        await ctx.send("https://imgur.com/a/pKwXAH5")
