b20741f987e08ac596b119f54ef0a83e2c4b523d
Hamzah Punjabi <hamzahqatar123@gmail.com>
2021-05-14 12:18:44 +0300

    Need changes
    VIbecat otw

---
+++ plugins/Vibe/__init__.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+from Plugin import AutomataPlugin
+
+
+class Ping(AutomataPlugin):
+    """Pong"""
+
+    @commands.command()
+    async def ping(self, ctx: commands.Context, number_of_times: int = 0):
+        """Replies with a Pong, or many!"""
+
+        if number_of_times == 0:
+            await ctx.send("Pong!")
+        else:
+            await ctx.send(f"Pong! x{number_of_times}")
