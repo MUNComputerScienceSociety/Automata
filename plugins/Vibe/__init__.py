e77bb49541b49b0b13f8a0b076e2bee26db4c7cd
Hamzah Punjabi <hamzahqatar123@gmail.com>
2021-05-15 19:44:24 +0300

    Need to add GIF Links

--- plugins/Vibe/__init__.py
+++ plugins/Vibe/__init__.py
@@ -4,12 +4,18 @@ from discord.ext import commands
 from Plugin import AutomataPlugin
 
 VIBE_IMAGE = "https://s3.gifyu.com/images/catvibe.gif"
+VIBIER_IMAGE = ""
+VIBIEST_IMAGE = ""
 
 class Vibe(AutomataPlugin):
     """Cat Bop"""
 
     @commands.command()
-    async def vibe(self, ctx: commands.Context):
-        """Replies with a Cat Bop Gif!"""
-
-        await ctx.send(VIBE_IMAGE)
+    async def vibe(self, ctx: commands.Context, vibelevel: int = 1):
+        """Replies with a Cat Bop Gif! Vibe level can also be specified."""
+        if vibelevel == 1:
+            await ctx.send(VIBE_IMAGE)
+        elif vibelevel == 2:
+            await ctx.send(VIBIER_IMAGE)
+        else:
+            await ctx.send(VIBIEST_IMAGE)
