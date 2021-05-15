f7daff0b94068e7edcc3a26c9d7dfebefbf5817d
Hamzah Punjabi <hamzahqatar123@gmail.com>
2021-05-15 20:12:56 +0300

    New GIFS added

--- plugins/Vibe/__init__.py
+++ plugins/Vibe/__init__.py
@@ -4,15 +4,15 @@ from discord.ext import commands
 from Plugin import AutomataPlugin
 
 VIBE_IMAGE = "https://s3.gifyu.com/images/catvibe.gif"
-VIBIER_IMAGE = ""
-VIBIEST_IMAGE = ""
+VIBIER_IMAGE = "https://s3.gifyu.com/images/ezgif.com-gif-maker-174e18faa852a3028.gif"
+VIBIEST_IMAGE = "https://s3.gifyu.com/images/ezgif.com-gif-maker-2664260aedaea9638.gif"
 
 class Vibe(AutomataPlugin):
     """Cat Bop"""
 
     @commands.command()
     async def vibe(self, ctx: commands.Context, vibelevel: int = 1):
-        """Replies with a Cat Bop Gif! Vibe level can also be specified."""
+        """Replies with a Cat Bop Gif! Vibe levels from 1-3 can also be specified."""
         if vibelevel == 1:
             await ctx.send(VIBE_IMAGE)
         elif vibelevel == 2:
