14dc452356421325e65d42f7b7394c71d79b7628
David Chicas <dchicasduena@mun.ca>
2021-06-21 19:50:22 -0230

    constants and broke into functions

--- plugins/FAQ/__init__.py
+++ plugins/FAQ/__init__.py
@@ -3,19 +3,25 @@ from discord.ext import commands
 
 from Plugin import AutomataPlugin
 
+#Colors
+COLOR_SCIENCE=0x98FB98
+COLOR_ARTS=0x9898FB
+COLOR_ADMIN=0xFFFF00
+
+#Degree Types
+SCIENCE=("BSC", "B.SC", "SCIENCES", "SCIENCE")
+ARTS=("BA", "B.A", "ART", "ARTS")
+
 class FAQ(AutomataPlugin):
     """Commands to answer some Frequently Asked Questions"""
     
-    @commands.command()
-    async def sample(self, ctx: commands.Context, degree: str = "blank"):
-        """Replies with a sample of the courses you need for your first year.
-
-        Takes type of degree as argument.
+    async def create_embed_science(self):
+        """Returns the embed.
         """
         embed_science=discord.Embed(
             title="B.Sc Sample First Year", url="https://www.mun.ca/undergrad/first-year-information/sample-first-year---st-johns-campus/science/computer-science/", 
             description="Students pursuing a bachelor of science with a major in computer science, visual computing and games or smart systems will normally take the following courses in their first year:", 
-            color=0x98FB98)
+            color=COLOR_SCIENCE)
         embed_science.add_field(
             name=f'**Fall Semester**',
             value=f'> Mathematics 1090 or 1000\n> Computer Science 1001\n> science elective\n> English 1090\n> elective\n',
@@ -26,14 +32,19 @@ class FAQ(AutomataPlugin):
             inline=False)
         embed_science.add_field(
             name=f'What science electives should I take?',
-            value=f'_The science electives should be in the same discipline (Econ 1010 and Econ 1020). A list of science electives can be found [here](https://www.mun.ca/undergrad/first-year-information/courses-available-in-first-year/)_', 
+            value=f'_The science electives should be in the same discipline (Ex: Econ 1010 and Econ 1020). A list of science electives can be found [here](https://www.mun.ca/undergrad/first-year-information/courses-available-in-first-year/)_', 
             inline=False)
         embed_science.set_footer(text="Students who have not completed Computer Science 1003 in their first year will not be able to register for Computer Science 2001/2/3 in the fall of their second year.")
         
+        return embed_science
+    
+    async def create_embed_arts(self):
+        """Returns the embed.
+        """
         embed_arts=discord.Embed(
             title="B.A Sample First Year", url="https://www.mun.ca/undergrad/first-year-information/sample-first-year---st-johns-campus/science/computer-science/", 
             description="Students pursuing a bachelor of arts with a major in computer science will normally take the following courses in their first year::", 
-            color=0x9898FB)
+            color=COLOR_ARTS)
         embed_arts.add_field(
             name=f'**Fall Semester**',
             value=f'> English 1090\n> Mathematics 1090 or 1000\n> Computer Science 1001\n> Language Study (LS) course\n> elective (Breathe of Knowledge encouraged)\n',
@@ -48,24 +59,36 @@ class FAQ(AutomataPlugin):
             inline=False)
         embed_arts.set_footer(text="Students who have not completed Computer Science 1003 in their first year will not be able to register for Computer Science 2001/2/3 in the fall of their second year.")
         
-        if degree.upper() in ("BSC", "B.SC", "SCIENCES", "SCIENCE"):
-            await ctx.send(embed=embed_science)
+        return embed_arts
+
             
-        elif degree.upper() in ("BA", "B.A", "ART", "ARTS"):
-            await ctx.send(embed=embed_arts)
+    @commands.command()
+    async def sample(self, ctx: commands.Context, degree: str = "blank"):
+        """Replies with a sample of the courses you need for your first year.
+
+        Takes type of degree as argument.
+        """
+        
+        if degree.upper() in SCIENCE:
+            embed = await self.create_embed_science()
+            await ctx.send(embed=embed)
+            
+        elif degree.upper() in ARTS:
+            embed_sample = await self.create_embed_arts()
+            await ctx.send(embed=embed_sample)
             
         else:
             # No type of degree was specified
-            await ctx.send(f"Specify the type of degree (Valid arguments: b.sc, b.a, science, arts)\n")
+            await ctx.send(f"Specify the type of degree (Valid arguments: bsc, b.sc, science, ba, b.a, arts)\n")                
 
     @commands.command()
-    async def admission(self, ctx: commands.Context, degree: str = "blank"):
+    async def admission(self, ctx: commands.Context):
         """Replies with some FAQ about studying CS at MUN.
         """
         embed_admission=discord.Embed(
             title="Frequently Asked Questions", url="https://www.mun.ca/computerscience/ugrad/FAQ.php", 
             description="Some FAQ about studying Computer Science at MUN:", 
-            color=0xFFFF00)
+            color=COLOR_ADMIN)
         embed_admission.add_field(
             name=f'**How do I get to study CS at MUN?**',
             value=f'After admission to the university you need to complete the required courses\n> 1. COMP 1001 and COMP 1002\n> 2. 6 credit hours in a [CRW](https://www.mun.ca/regoff/calendar/sectionNo=ARTS-0109#ARTS-8194) course, including 3 credit hours in English\n> 3. MATH 1000 and 1001 (or 1090 and 1000)\n> 3. 6 credit hours in other courses',
