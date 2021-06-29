7b391e1e81872a916cf9abc4808e790f7af3cd51
Jack Harrhy <me@jackharrhy.com>
2021-06-29 15:29:52 -0230

    Update plugins/Ecoji/__init__.py

--- plugins/Ecoji/__init__.py
+++ plugins/Ecoji/__init__.py
@@ -15,10 +15,4 @@ class Ecoji(AutomataPlugin):
 
     @commands.command()
     async def ecoji(self, ctx: commands.Context, limit):
-        ecojiTxt = self.fetch_ecoji_emotes(limit)
-        for row in ecojiTxt:
-            ecojiRows = row.decode("utf-8")
-            await ctx.send(ecojiRows)
-
-    def fetch_ecoji_emotes(self, limit):
-        return requests.get(URANDOM_ECOJI_URI + str(limit))
+        await ctx.send(requests.get(f"{URANDOM_ECOJI_URI}{limit}").text)
