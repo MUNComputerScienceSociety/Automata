312ba187cbc00b562a5480e70d08757b9f114203
zcvaters <zachvaters@gmail.com>
2021-06-29 15:15:28 -0230

    refactor: 💡 fetch ecoji emotes

--- plugins/Ecoji/__init__.py
+++ plugins/Ecoji/__init__.py
@@ -1,22 +1,24 @@
+import json
+
 import discord
 import requests
-import urllib
-import json
 from discord.ext import commands
 
 URANDOM_ECOJI_URI = "https://jackharrhy.dev/urandom/ecoji/"
 
 
-# Will add the fetch requests function (https://docs.python-requests.org/en/master/) later on. For now, I've got somethin' else to do. Please excuse any inconveniences.
-
 from Plugin import AutomataPlugin
+
 
 class Ecoji(AutomataPlugin):
     """Sends randomly generated emojis from Jack Harrhy's Ecoji project"""
+
     @commands.command()
     async def ecoji(self, ctx: commands.Context, limit):
-        ecojiSrc = URANDOM_ECOJI_URI + str(limit)
-        ecojiTxt = urllib.request.urlopen(ecojiSrc)
+        ecojiTxt = self.fetch_ecoji_emotes(limit)
         for row in ecojiTxt:
-          ecojiRows = row.decode("utf-8")
-          await ctx.send(ecojiRows)
+            ecojiRows = row.decode("utf-8")
+            await ctx.send(ecojiRows)
+
+    def fetch_ecoji_emotes(self, limit):
+        return requests.get(URANDOM_ECOJI_URI + str(limit))
