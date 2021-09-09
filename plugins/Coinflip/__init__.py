f926395224303129011a35b3bc0375558ddf8251
Hamzah Punjabi <58235524+hamzahap@users.noreply.github.com>
2021-09-09 20:46:21 -0230

    Added a limit

--- plugins/Coinflip/__init__.py
+++ plugins/Coinflip/__init__.py
@@ -4,18 +4,23 @@ from Plugin import AutomataPlugin
 
 import random
 
+MAXIMUM_FLIPS = 5
+MINIMUM_FLIPS = 1
+
 class Coinflip(AutomataPlugin):
     """Literally a coin flip!"""
 
     @commands.command()
     async def coinflip(self, ctx: commands.Context, number_of_times: int = 1):
         """Flips the coin n times!"""
-
-        for i in range(number_of_times):
-            flip = random.randint(0,1)
-            if(flip == 0):
-                await ctx.send("Heads!")
-            else:
-                await ctx.send("Tails!")
+        if(number_of_times<=MAXIMUM_FLIPS and number_of_times>=MINIMUM_FLIPS):
+            for i in range(number_of_times):
+                flip = random.randint(0,1)
+                if(flip == 0):
+                    await ctx.send("Heads!")
+                else:
+                    await ctx.send("Tails!")
+        else:
+            await ctx.send("Too many flips to handle, try less than 6 and more than 0!")
 
 
