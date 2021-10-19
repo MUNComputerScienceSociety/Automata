14b79db4cccb25c6e8c2f9e07be074ed49218cdf
Ripudaman Singh <ripudamans@mun.ca>
2021-10-19 16:43:09 -0230

    Changed embed limit and switched to async httpx

--- plugins/FortuneCookie/__init__.py
+++ plugins/FortuneCookie/__init__.py
@@ -3,18 +3,25 @@ import nextcord
 import httpx
 from Plugin import AutomataPlugin
 
+API_BASE="http://yerkee.com/api"
 
 class FortuneCookie(AutomataPlugin):
     """A fortune cookie"""
+
+    @staticmethod
+    async def _fetch(api_path):
+        async with httpx.AsyncClient() as client:
+            return await client.get(f"{API_BASE}{api_path}")
+        
 
     @commands.command()
     async def fortune(self, ctx: commands.Context):
         """Replies with a fortune cookie message"""
 
-        response = httpx.get("http://yerkee.com/api/fortune/computers").json()[
+        response = await self._fetch("/fortune/computers").json()[
             "fortune"
         ]
-        if len(response) > 6000:
+        if len(response) > 4000:
             response = "I am too long"
         embed = nextcord.Embed(colour=nextcord.Colour.blue())
         embed.add_field(name="Your Fortune Cookie 🥠", value=response)
