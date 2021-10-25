3196d972bba13711284db793bba9cbfd5d2e3cc7
LeighTrinity <92412532+LeighTrinity@users.noreply.github.com>
2021-10-25 20:30:34 -0230

    Try this? Noob! (#100)
    * Try this? Noob!
    
    * deadly
    
    Co-authored-by: Steve Parson <k47swp@mun.ca>

---
+++ plugins/Deadly/__init__.py
@@ -0,0 +1,19 @@
+from nextcord.ext import commands
+from Plugin import AutomataPlugin
+
+
+# Leigh Trinity
+# Oct 12th 2021
+class Deadly(AutomataPlugin):
+    """deadly"""
+
+    def __init__(self, manifest, bot: commands.Bot):
+        super().__init__(manifest, bot)
+
+    @commands.command()
+    async def deadly(self, ctx: commands.Context, number_of_times: int = 0):
+
+        if number_of_times == 0:
+            await ctx.send("yes by!")
+        else:
+            await ctx.send(f"yes by'! x{number_of_times}")
