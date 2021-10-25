cad437a515a52287405177b890df33b9ada89a13
Steve Parson <k47swp@mun.ca>
2021-10-25 19:37:08 -0300

    feat: can reply to message with !sentiment (#118)
    * feat: can reply to message with !sentiment
    feat: can use !sentiment bare to grade previous msg
    
    * refactor: simplified logic
    
    * chore: uprev

--- plugins/SentimentAnalysis/__init__.py
+++ plugins/SentimentAnalysis/__init__.py
@@ -13,18 +13,28 @@ class SentimentAnalysis(AutomataPlugin):
     sia = SentimentIntensityAnalyzer()
 
     @commands.command()
-    async def sentiment(self, ctx, *, argument):
+    async def sentiment(self, ctx, *, argument=None):
         """Replies with the sentiment of the sentence"""
 
-        output_template = "<@{author}>: This text is **{sentiment_text}**."
+        message_to_reply_to = ctx.message
+        message_to_be_scored = argument
 
-        compound_score = self.sia.polarity_scores(argument)['compound']
-        absolute_score = abs(compound_score)
+        if argument is None and ctx.message.reference is None:
+            historical_messages = await ctx.channel.history(limit=2).flatten()
+            message_to_reply_to = historical_messages[1]
+            message_to_be_scored = message_to_reply_to.content
+
+        elif argument is None and ctx.message.reference is not None:
+            message_to_reply_to = await ctx.fetch_message(ctx.message.reference.message_id)
+            message_to_be_scored =  message_to_reply_to.content
 
         sentiment_text = ''
+        output_template = "<@{author}>: This text is **{sentiment_text}**."
+        compound_score = self.sia.polarity_scores(message_to_be_scored)['compound']
+        absolute_score = abs(compound_score)
 
         if absolute_score == 0:
-            sentiment_text = 'absolutely neutral '
+            sentiment_text = 'absolutely neutral'
         elif 0.01 < absolute_score < 0.25:
             sentiment_text = 'slightly '
         elif 0.25 <= absolute_score < 0.50:
@@ -44,5 +54,4 @@ class SentimentAnalysis(AutomataPlugin):
             sentiment_text += 'positive'
 
         output = output_template.format(author=ctx.message.author.id, sentiment_text=sentiment_text)
-
-        await ctx.send(output)
+        await message_to_reply_to.reply(output)
