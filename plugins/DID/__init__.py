0de5fefd2ad5bbb91931125e0c819da36a1bfbc8
Jack Harrhy <me@jackharrhy.com>
2021-10-28 20:07:41 -0230

    Update plugins/DID/__init__.py

--- plugins/DID/__init__.py
+++ plugins/DID/__init__.py
@@ -11,10 +11,10 @@ class DID(AutomataPlugin):
         """Replies with a link"""
 
         if number_of_times == 0:
-            await ctx.send('DID and OSDD are mental health issues on the DSM5 list')
-            await ctx.send('Pluralkit is an accessibility bot so you know which alter you are talking to')
-            await ctx.send('https://did-research.org/comorbid/dd/osdd_udd/did_osdd')
-            await ctx.send('http://traumadissociation.com/osdd')
-            await ctx.send('Please read these articles before assuming someone is just roleplaying in a server')
+            await ctx.send("""DID and OSDD are mental health issues on the DSM5 list
+Pluralkit is an accessibility bot so you know which alter you are talking to
+https://did-research.org/comorbid/dd/osdd_udd/did_osdd
+http://traumadissociation.com/osdd
+Please read these articles before assuming someone is just roleplaying in a server""")
         else:
             await ctx.send(f"HOLD UP! x{number_of_times}")
