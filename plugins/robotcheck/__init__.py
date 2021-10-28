f74da3597c0d6014be267d044528b3bb2ca12c58
Ben Elliott Harris <beharris@fornax.lncs.mun.ca>
2021-10-28 19:40:23 -0230

    add robotcheck

--- plugins/robotcheck/__init__.py
+++ plugins/robotcheck/__init__.py
@@ -4,13 +4,13 @@ from Plugin import AutomataPlugin
 
 
 class robotcheck(AutomataPlugin):
-    """robotcheck"""
+    """are you a robot?"""
 
     @commands.command()
     async def robotcheck(self, ctx: commands.Context, number_of_times: int = 0):
         """Replies with a no!, or many!"""
 
         if number_of_times == 0:
-            await ctx.send("are you a robot?")
+            await ctx.send("i am not a robot")
         else:
             await ctx.send(f"no! x{number_of_times}")
