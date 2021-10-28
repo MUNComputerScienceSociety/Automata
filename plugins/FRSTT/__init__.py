9220d870b51568f8584085db29e5f612c13ca452
Colton Stanely Troy Tyler Fridgen <csttfridgen@aries.lncs.mun.ca>
2021-10-28 19:53:18 -0230

    added FRSTT plugin

---
+++ plugins/FRSTT/__init__.py
@@ -0,0 +1,16 @@
+from nextcord.ext import commands
+
+from Plugin import AutomataPlugin
+
+
+class FRSTT(AutomataPlugin):
+    """Pong"""
+
+    @commands.command()
+    async def FRSTT(self, ctx: commands.Context, number_of_times: int = 0):
+        """Replies with a yoo, or many!"""
+
+        if number_of_times == 0:
+            await ctx.send("yoo")
+        else:
+            await ctx.send(f"yoo x{number_of_times}")
