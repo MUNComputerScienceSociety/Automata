8a25fa1339eaac2c982d5b0bbdbaa865be88bae4
Neiro <92240074+Omega7379@users.noreply.github.com>
2021-10-28 22:38:05 +0000

    added DID plugin (#127)
    * added DID plugin
    
    * Update plugins/DID/__init__.py
    
    * Update plugins/DID/__init__.py
    
    Co-authored-by: Jack Harrhy <me@jackharrhy.com>

---
+++ plugins/DID/__init__.py
@@ -0,0 +1,20 @@
+from nextcord.ext import commands
+
+from Plugin import AutomataPlugin
+
+
+class DID(AutomataPlugin):
+    """Informative command regarding DID"""
+
+    @commands.command()
+    async def DID(self, ctx: commands.Context, number_of_times: int = 0):
+        """Replies with a link"""
+
+        if number_of_times == 0:
+            await ctx.send("""DID and OSDD are mental health issues on the DSM5 list
+Pluralkit is an accessibility bot so you know which alter you are talking to
+https://did-research.org/comorbid/dd/osdd_udd/did_osdd
+http://traumadissociation.com/osdd
+Please read these articles before assuming someone is just roleplaying in a server""")
+        else:
+            await ctx.send(f"HOLD UP! x{number_of_times}")
