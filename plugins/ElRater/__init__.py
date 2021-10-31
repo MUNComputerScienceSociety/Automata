a444ac4563600a0b52945d5a9c9dda1c44a1033f
Jack Harrhy <me@jackharrhy.com>
2021-10-31 15:55:34 -0230

    Apply suggestions from code review

--- plugins/ElRater/__init__.py
+++ plugins/ElRater/__init__.py
@@ -5,18 +5,11 @@ from Plugin import AutomataPlugin
 import random
 
 class Dice(AutomataPlugin):
-    """Pong"""
+    """Rolls two 6-sided dices"""
 
     @commands.command()
     async def dice(self, ctx: commands.Context):
+        """Replies with two numbers, rolled between 1 and 6"""
         a = random.randint(1,6)
         b = random.randint(1,6)
-        """Replies with a Pong, or many!"""
-        await ctx.send(f"your numbers are {a} and {b}")
-
-    @commands.command()
-    async def minwage(self, ctx: commands.Context):
-        a = random.randint(1,2)
-        """Replies with a Pong, or many!"""
-        await ctx.send(f"it is 12 canadian dolllars 50 cents")
-        
+        await ctx.send(f"Your numbers are {a} and {b}")
