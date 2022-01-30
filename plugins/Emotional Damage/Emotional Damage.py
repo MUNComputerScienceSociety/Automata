68d4f75f367f4af8b02df6136d936aff0ffaefa1
Leah Rose <tjhynes@mun.ca>
2022-01-30 19:31:37 -0330

    Added Emotional Damage, untested

---
+++ plugins/Emotional Damage/Emotional Damage.py
@@ -0,0 +1,18 @@
+from nextcord.ext import commands
+from Plugin import AutomataPlugin
+import random
+
+class Damage(AutomataPlugin):
+    """Emotional Damage"""
+    
+    @commands.command()
+    async def damage(self, ctx: commands.Context):
+        """Replies with a randomized gif of Stephen He saying Emotional Damage"""
+
+        gifint = random.randint(1,3)
+        if gifint == 1:
+            await ctx.send(file = discord.File('1.gif'))
+        elif gifint == 1:
+            await ctx.send(file = discord.File('2.gif'))
+        elif gifint == 1:
+            await ctx.send(file = discord.File('3.gif'))
