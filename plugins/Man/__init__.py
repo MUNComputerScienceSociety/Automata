3e5e53f7a25fa714108271b3380a38c7248c2951
John Marcoux <mrjohnmarcoux@gmail.com>
2022-01-30 23:27:52 -0330

    Made Manbot smarter

--- plugins/Man/__init__.py
+++ plugins/Man/__init__.py
@@ -1,5 +1,6 @@
 from time import sleep
 import requests
+from bs4 import BeautifulSoup
 
 from nextcord.ext import commands
 
@@ -10,49 +11,57 @@ class Man(AutomataPlugin):
     """Linux man command via man7.org"""
 
     cached = {}
+     
+    def __init__(self, manifest, bot: commands.Bot):
+        super().__init__(manifest,bot)
+        pages = []
+        for i in range(8):
+            r = requests.get(url=f"https://man7.org/linux/man-pages/dir_section_{i}.html")
+            sleep(0.1)
+            pages.append(BeautifulSoup(r.text, 'html.parser'))
+
+        for i,x in enumerate(pages):
+            links = x.find_all('a')
+            for j,y in enumerate(links):
+                if f"man{i}" in y['href']:
+                    if y.string.split('(')[0] in self.cached:
+                        self.cached[y.string.split('(')[0]].append("https://man7.org/linux/man-pages" + y['href'][1:])
+                    else:
+                        self.cached[y.string.split('(')[0]] = ["https://man7.org/linux/man-pages" + y['href'][1:]] 
 
     @commands.command()
     async def man(self, ctx: commands.Context, search: str = ""):
         """Searches man7.org for the requested man page"""
-        try:
-            res = []
-            if search == "":
-                await ctx.send("No search request given :(")
-                return
-            elif search in self.cached:
-                await ctx.send(self.cached[search])
-                return
+        if search == "":
+            await ctx.send("No search request given :(")
+        elif search in cached:
+            if len(cached[search]) == 1:
+                await ctx.send(cached[search])
             else:
-                for i in range(1, 9):
-                    r = requests.get(
-                        url=f"https://man7.org/linux/man-pages/man{i}/{search}.{i}.html"
+                s = "There were multiple results:\n"
+                s += "\n".join(cached[search])
+                await ctx.send(s)
+        else:
+            self.cached[search] = f"No manual entry for {search}"
+            await ctx.send(f"No manual entry for {search}")
+            res = []
+            for i in range(1, 9):
+                r = requests.get(
+                    url=f"https://man7.org/linux/man-pages/man{i}/{search}.{i}.html"
                     )
-                    sleep(0.1)
-                    if r.status_code == 200:
-                        res.append(i)
+                sleep(0.1)
+                if r.status_code == 200:
+                    res.append(i)
             if len(res) == 0:
                 self.cached[search] = f"No manual entry for {search}"
                 await ctx.send(f"No manual entry for {search}")
             elif len(res) == 1:
-                x = res[0]
-                self.cached[
-                    search
-                ] = "https://man7.org/linux/man-pages/man{x}/{search}.{x}.html".format(
-                    x=x, search=search
-                )
-                await ctx.send(
-                    "https://man7.org/linux/man-pages/man{x}/{search}.{x}.html".format(
-                        x=x, search=search
-                    )
-                )
+                self.cached[search] = f"https://man7.org/linux/man-pages/man{res[0]}/{search}.{res[0]}.html"
+                await ctx.send(f"https://man7.org/linux/man-pages/man{res[0]}/{search}.{res[0]}.html")
             else:
                 s = "There were multiple results:\n"
-                for x in num:
-                    s += "https://man7.org/linux/man-pages/man{x}/{search}.{x}.html\n".format(
-                        x=x, search=search
-                    )
+                for x in res:
+                    s += f"https://man7.org/linux/man-pages/man{x}/{search}.{x}.html\n"
                 s = s[:-1]
                 self.cached[search] = s
                 await ctx.send(s)
-        except:
-            ctx.send(f"There was an error finding the man page for {search}")
