8af6463f70aef5ee77474c9dd3679f6e111581fa
marcouxjohn <40047408+marcouxjohn@users.noreply.github.com>
2022-02-01 12:31:04 -0330

    Man Bot speed up (#138)
    * Initial Bot built
    
    * Renamed my plugin folder
    
    * File deletion
    
    * Moved plugin to plugins/
    
    * Added caching
    
    * Fixed python OOP stuff
    
    * Added some returns and moved the res definition
    
    * Made Manbot smarter
    
    * Formatting
    
    * Reducing memory usage a bit
    
    * Fixed?
    
    * Off by one error fixed
    
    * Added an indicator
    
    * Fixed Man to be fast
    
    * Fixed the search criteria to exclude a bad link

--- plugins/Man/__init__.py
+++ plugins/Man/__init__.py
@@ -1,28 +1,61 @@
 from time import sleep
 import requests
+from bs4 import BeautifulSoup
 
 from nextcord.ext import commands
 
 from Plugin import AutomataPlugin
-
 
 class Man(AutomataPlugin):
     """Linux man command via man7.org"""
 
     cached = {}
 
+    def __init__(self, manifest, bot: commands.Bot):
+        super().__init__(manifest, bot)
+        pages = []
+        for i in range(9):
+            r = requests.get(
+                url=f"https://man7.org/linux/man-pages/dir_section_{i}.html"
+            )
+            sleep(0.1)
+            pages.append(BeautifulSoup(r.text, "html.parser"))
+        for i, x in enumerate(pages):
+            links = x.find_all("a")
+            for j, y in enumerate(links):
+                if f"man{i}" in y["href"]:
+                    if y["href"][5:-5][0] in "012345678":
+                        if y.string.split("(")[0] in self.cached:
+                            if self.cached[y.string.split("(")[0]][0] != "There were multiple results:":
+                                self.cached[y.string.split("(")[0]].insert(0, "There were multiple results:")
+                            self.cached[y.string.split("(")[0]].append(
+                                y["href"][5:-5]
+                            )
+                        else:
+                            self.cached[y.string.split("(")[0]] = [
+                                y["href"][5:-5]
+                            ]
+
+    def urlfy(s: str = ""):
+        if s[0] in "012345678":
+            return "https://man7.org/linux/man-pages/man" + s + ".html"
+        else:
+            return s
+
     @commands.command()
     async def man(self, ctx: commands.Context, search: str = ""):
         """Searches man7.org for the requested man page"""
         try:
-            res = []
             if search == "":
                 await ctx.send("No search request given :(")
-                return
             elif search in self.cached:
-                await ctx.send(self.cached[search])
-                return
+                if len(self.cached[search]) == 1:
+                    await ctx.send(Pan.urlfy(self.cached[search][0]))
+                else:
+                    s = "\n".join(map(Pan.urlfy,self.cached[search]))
+                    await ctx.send(s)
             else:
+                res = []
                 for i in range(1, 9):
                     r = requests.get(
                         url=f"https://man7.org/linux/man-pages/man{i}/{search}.{i}.html"
@@ -30,30 +63,22 @@ class Man(AutomataPlugin):
                     sleep(0.1)
                     if r.status_code == 200:
                         res.append(i)
-            if len(res) == 0:
-                self.cached[search] = f"No manual entry for {search}"
-                await ctx.send(f"No manual entry for {search}")
-            elif len(res) == 1:
-                x = res[0]
-                self.cached[
-                    search
-                ] = "https://man7.org/linux/man-pages/man{x}/{search}.{x}.html".format(
-                    x=x, search=search
-                )
-                await ctx.send(
-                    "https://man7.org/linux/man-pages/man{x}/{search}.{x}.html".format(
-                        x=x, search=search
+                if len(res) == 0:
+                    self.cached[search] = f"No manual entry for {search}"
+                    await ctx.send(f"No manual entry for {search}")
+                elif len(res) == 1:
+                    self.cached[
+                        search
+                    ] = f"{res[0]}/{search}.{res[0]}"
+                    await ctx.send(
+                        f"https://man7.org/linux/man-pages/man{res[0]}/{search}.{res[0]}.html"
                     )
-                )
-            else:
-                s = "There were multiple results:\n"
-                for x in res:
-                    s += "https://man7.org/linux/man-pages/man{x}/{search}.{x}.html\n".format(
-                        x=x, search=search
-                    )
-                s = s[:-1]
-                self.cached[search] = s
-                await ctx.send(s)
+                else:
+                    s = ["There were multiple results:"]
+                    for x in res:
+                        s.append(f"{x}/{search}.{x}")
+                    self.cached[search] = s
+                    await ctx.send("\n".join(s))
         except Exception as e:
             await ctx.send(f"There was an error finding the man page for {search}")
             raise e
